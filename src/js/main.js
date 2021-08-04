0286ee0225450c9d0de72e7fd39fc3708c17bbe5
a-klimovich <ar2r.klimovich@gmail.com>
2021-08-04 21:00:58 +0300

    finish: desktop

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
 (function() {
-  alert('hola')
-})
+  const $header = $(".header");
+  const scroll = 0;
+  const active = "active";
+  
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > scroll) {
+      $header.addClass(active);
+    } else {
+      $header.removeClass(active);
+    }
+  });
+
+  $('.hamburger').on('click', () => {
+    $('.hamburger').toggleClass('active')
+    $('.menu').toggleClass('active')
+  });
+})()
